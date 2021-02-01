139e69f52ca2572f5a70e62458918f05eca0c16a
Andrey <andrey.dmr@gmail.com>
2021-02-01 18:14:28 +0300

    Initial Commit

---
+++ The Light 2.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  The Light 2.0
+//
+//  Created by Andrey on 01.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
